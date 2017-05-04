b98d646c7d0ee6aeb45d0ca3874ae4919d69e6af
shiptest-rc-me <shiptest.rc.me@gmail.com>
2017-05-04 08:02:54 +0000

    echo.sh edited online with Bitbucket

--- echo.sh
+++ echo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 echo "testing runshtrigger!!!"
 echo SAMPLE_SCRIPT=res1
-echo $SAMPLE_SCRIPT_IS_FORK
-echo $SAMPLE_SCRIPT_IS_PULL_REQUEST
-echo $SAMPLE_SCRIPT_REPO_FULL_NAME
-echo $SAMPLE_SCRIPT_PULL_REQUEST_REPO_FULL_NAME
+echo $res1_IS_FORK
+echo $res1_IS_PULL_REQUEST
+echo $res1_REPO_FULL_NAME
+echo $res1_PULL_REQUEST_REPO_FULL_NAME
